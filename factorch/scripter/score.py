ddfd2758253a11d783a9b50b156897b51bfeb989
Sandy Chen <silentchaos125@outlook.com>
2022-05-17 09:10:39 +0800

    Modify scoring method

--- factorch/scripter/score.py
+++ factorch/scripter/score.py
@@ -30,10 +30,7 @@ def check_prediction(factor, factor_value, length, d, group_num=20):
     ret_v2v_1d = (
         load_daily(field='vwap', valid_field=('is_valid_raw',)) * load_daily(field='adjfactor')
     ).pct_change().shift(-2).sort_index(axis=0) * 100
-    # ret_v2v_1d = ret_v2v_1d[ret_v2v_1d.index.isin(fval_dates)]
     is_buyable = load_daily('vwap') < load_daily('limit_up_price')
-
-    # fval = fval.loc[:is_buyable.index[-length-1]].stack()
 
     excrets = {}
     w = np.array([d**i for i in range(length)])  # Exponential weights, e.g. np.array([[1], [0.9], [0.81], ...])
@@ -53,6 +50,5 @@ def check_prediction(factor, factor_value, length, d, group_num=20):
     fval = fval.reindex(excrets.index).groupby(level=0).apply(
         lambda x: pd.qcut(x.rank(ascending=False, method='first'), q=group_num, labels=range(1, group_num+1))
     )  # Ranking and binning factor value
-    excess_return = excrets.groupby([fval.index.get_level_values(0), fval]).mean()
-    return excess_return
+    return excrets.groupby([fval.index.get_level_values(0), fval]).mean()
     
